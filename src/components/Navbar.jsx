797637b0f2d8a40509ff6cdd57467c75eeac48e0
Jaewon Han <jaewonhan20@gmail.com>
2023-12-04 00:02:24 -0600

    feat: Add NavBar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header>
-      <div>Junyong & Jaewon</div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/our-story">Our Story</Link>
-        <Link to="/gallery">Gallery</Link>
-        <Link to="/guest-book">Guest Book</Link>
-        <Link to="/rsvp">RSVP</Link>
-        <Link to="/upload">Upload</Link>
-      </nav>
+    <header className="fixed top-0 left-0 w-full bg-black text-white z-10">
+      <div className="flex flex-col sm:flex-row justify-between items-center p-4 max-w-6xl mx-auto">
+        <div className="flex justify-between items-center w-full sm:w-auto">
+          <div className="text-xl font-bold">Junyong & Jaewon</div>
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="text-xl hover:text-gray-300 sm:hidden"
+          >
+            {isOpen ? "X" : "☰"}
+          </button>
+        </div>
+        <nav
+          className={`${
+            isOpen ? "flex" : "hidden"
+          } flex-col sm:flex sm:flex-row gap-4 items-center w-full sm:w-auto`}
+        >
+          <Link to="/" className="hover:text-gray-300">
+            Home
+          </Link>
+          <Link to="/our-story" className="hover:text-gray-300">
+            Our Story
+          </Link>
+          <Link to="/gallery" className="hover:text-gray-300">
+            Gallery
+          </Link>
+          <Link to="/guest-book" className="hover:text-gray-300">
+            Guest Book
+          </Link>
+          <Link to="/rsvp" className="hover:text-gray-300">
+            RSVP
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 }
